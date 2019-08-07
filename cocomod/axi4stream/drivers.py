138a554f6b693c69a4ad5eb483346aea0ec31968
Fabio Garzetti <fabio.garzetti@polimi.it>
2019-08-07 10:06:06 +0200

    Add n_bit property

--- cocomod/axi4stream/drivers.py
+++ cocomod/axi4stream/drivers.py
@@ -117,6 +117,9 @@ class Axi4StreamMaster(BusDriver):
 
         self.bus.TVALID <= 0
 
+    @property
+    def n_bits(self):
+        return self.bus.TDATA.value.n_bits
 
 class Axi4StreamSlave(BusDriver):
     """
