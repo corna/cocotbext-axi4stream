30edb9b65851e6281c4a875973c3c677f331b1ef
Nicola Corna <nicola@corna.info>
2020-07-22 19:24:53 +0200

    Drop Python 2 support

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
-#! /usr/bin/python
-from setuptools import setup
+#!/usr/bin/env python3
+
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="cocotbext.axi4stream",
-    use_scm_version={
-        "relative_to": __file__,
-        "write_to": "cocotbext/axi4stream/version.py",
-    },
+    version="1.0",
     author="Nicola Corna",
     author_email="nicola.corna@polimi.it",
     description="Cocotb AXI4-Stream module",
     url="https://github.com/corna/cocotbext.axi4stream.git",
-    packages=["cocotbext.axi4stream"],
+    packages=find_namespace_packages(include=['cocotbext.*']),
     install_requires=['cocotb'],
-    setup_requires=[
-        'setuptools_scm',
-    ],
+    python_requires='>=3.5',
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
